4b142c2ae73d1d4d4702ecff9d2a40a325625dbf
Kelvin Lu <kelvinlu@ucdavis.edu>
2016-04-06 19:41:20 -0700

    add resources from static site

--- storyspace/routes.py
+++ storyspace/routes.py
@@ -3,7 +3,7 @@ from storyspace import app, api, views
 
 
 # Static files
-app.add_url_rule(app.static_url_path + '/<path:filepath>', endpoint='static', view_func=views.StaticView.as_view('assets'))
+app.add_url_rule(app.static_url_path + '/<path:filepath>', view_func=views.StaticView.as_view('assets'))
 
 # Uploads
 app.add_url_rule('/uploads/images/<path:filename>', view_func=views.UploadsImagesView.as_view('uploads_images'))
